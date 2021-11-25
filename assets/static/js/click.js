3a1db83a5285658a22c6ec1233e99ae0bcedbdea
ying <ohis.yingshiuan@gmail.com>
2021-11-25 21:04:23 +0800

    index

--- assets/static/js/click.js
+++
@@ -1,9 +0,0 @@
-
-/* Click Hide mennu */
-        $(document).ready(function(){
-  	        $('.navbarToggle').click(function(){
-    	        $('.item').toggleClass("show");
-   			    $('ul li').toggleClass("hide");
-        			
-             });
-         });
